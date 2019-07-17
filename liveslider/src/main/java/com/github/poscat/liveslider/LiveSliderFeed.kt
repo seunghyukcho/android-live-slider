69c5506e00f0ee7805caf3ddf128b61ddb4a9b87
shhj1998 <shhj1998@postech.ac.kr>
2019-07-18 01:33:37 +0900

    add explanations

--- liveslider/src/main/java/com/github/poscat/liveslider/LiveSliderFeed.kt
+++ liveslider/src/main/java/com/github/poscat/liveslider/LiveSliderFeed.kt
@@ -1,5 +1,10 @@
 package com.github.poscat.liveslider
 
+/**
+ * Feed containing *items*.
+ *
+ * @param T the type of the item in your Feed.
+ */
 class LiveSliderFeed<T> {
     var category = "example"
     var items : ArrayList<T>? = null
--- liveslider/src/main/java/com/github/poscat/liveslider/LiveSliderViewPager.kt
+++ liveslider/src/main/java/com/github/poscat/liveslider/LiveSliderViewPager.kt
@@ -5,6 +5,9 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.viewpager.widget.ViewPager
 
+/**
+ * This custom ViewPager can block scrolling through touch.
+ */
 class LiveSliderViewPager : ViewPager {
     private var disable = false
 
@@ -20,6 +23,11 @@ class LiveSliderViewPager : ViewPager {
         return if (disable) false else super.onTouchEvent(event)
     }
 
+    /**
+     * If you want to block scrolling, use this function.
+     *
+     * @property disable determines whether to scroll.
+     */
     fun disableScroll(disable: Boolean?) {
         // When disable = true not work the scroll and when disble = false work the scroll
         this.disable = disable!!
